5fc62ca71f7baab8cc24e3394e43a35de26d8c61
ManasaVishwanath <mansu.cool@gmail.com>
2014-12-01 23:48:03 +0530

    Commiting initial changes

---
+++ New_Patient.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+
+<link rel="stylesheet" type="text/css" href="dental.css">
+
+</head>
+<body>
+<div class="header">
+<h2> <b>Welcome to Dental Clinical Software </b></h2>
+</div>
+
+<div class="section">
+<img class="image" src="http://photo.elsoar.com/wp-content/images/Smiling-cartoon-teeth-in-dentist-cabinet.jpg">
+<div class="button-sec">
+
+
+<label>Patient Name</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<input class="def-input"><br /><br />
+
+	
+	
+    <label>Date Of Birth</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <input class="def-input"><br /><br />
+    <label>Mobile Number</label>&nbsp;&nbsp;
+    <input class="def-input"><br /><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <button class="def-button">Save</button> 
+   
+</div>
+</div>
+
+<div class="footer">
+Copiright
+</div>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+<link rel="stylesheet" type="text/css" href="dental.css">
+</head>
+
+<body>
+<div class="header">
+<h2> <b>Dental Clinical Software </b></h2>
+</div>
+
+<div class="section">
+<img class="image" src="http://photo.elsoar.com/wp-content/images/Smiling-cartoon-teeth-in-dentist-cabinet.jpg">
+<div class="button-sec">
+	
+   <label>User Name</label> <input class="def-input"><br /><br />
+     
+     <label>Password</label>&nbsp;&nbsp;  <input class="def-input"><br /><br />
+    <button class="def-button">Submit</button>
+</div>
+</div>
+
+<div class="footer">
+Copiright
+</div>
+</body>
+</html>
